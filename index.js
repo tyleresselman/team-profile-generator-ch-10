7e76b4e71841af8cf5b31fe8de5d6f7141e8aa76
Tyler Esselman <tme91302@gmail.com>
2022-09-27 19:33:18 -0700

    built question functions

---
+++ index.js
@@ -0,0 +1,125 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+const teamMembers = [];
+
+const managerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter name of Manager.',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Please enter ID of Manager.',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'Please enter email of Manager.',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'Please enter office number of Manager.',
+            name: 'officeNumber'
+        }
+    ])
+    .then((answers) => {
+        const newManager = new newManager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamMembers.push(newManager);
+    })
+};
+
+const engineerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter name of engineer.',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Please enter ID of engineer.',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'Please enter email of engineer.',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'Please enter link to GitHub page of engineer.',
+            name: 'github'
+        }
+    ])
+    .then((answers) => {
+        const newEngineer = new newEngineer(answers.name, answers.id, answers.email, answers.github);
+        teamMembers.push(newEngineer);
+
+    })
+};
+
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter name of intern.',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Please enter ID of intern.',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'Please enter email of intern.',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'Please enter school attended by intern.',
+            name: 'school'
+        }
+    ])
+    .then((answers) => {
+        const newIntern = new newIntern(answers.name, answers.id, answers.email, answers.school);
+        teamMembers.push(newIntern);
+    })
+};
+
+const newEntry = () => {
+    inquirer.prompt([
+        {
+            type:'list',
+            message: 'Would you like to add another member to the team? Or shall we create your team list?',
+            choices: ['Add a new engineer', 'Add a new intern', 'Create the team list'],
+            name: 'options'
+        }
+    ])
+    .then((answer) => {
+        switch(answer.options){
+            case 'Add a new engineer':
+                engineerQuestions();
+                break;
+            case 'Add a new intern':
+                internQuestions();
+                break;
+            default: 
+                createTeam();
+                break;
+
+        }
+    })
+
+};
+
